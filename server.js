6f1e76161e9c3ba526670010f6548e4162972447
Amine Affif <affif.amine@live.fr>
2021-09-18 16:52:40 +0200

    added login and register routes and forms inside

--- server.js
+++ server.js
@@ -1,20 +1,51 @@
 // Imports
 const express = require("express")
+const dotenv = require("dotenv")
+const path = require("path")
+
+dotenv.config({ path:"./.env" })
+
 
 // Instanciate server
 const app = express()
 
-const list = [
-    {
-        username: "amine affif",
-        email: "amine@affif.fr",
-        password: "r8d0h7498y4e9",
-    }
-]
+
+// Body Parser config
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+// Template Engine config
+const publicDirectory = path.join(__dirname, "./public")
+app.set("view engine", "hbs")
+app.use(express.static(publicDirectory))
+
 
 // Routes
-app.get("/list", (req, res) => {
-    res.json(list)
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/api/login", (req, res) => {
+    res.render("login")
+})
+app.post("/api/login", (req, res) => {
+    
+})
+
+app.get("/api/register", (req, res) => {
+    res.render("register")
+})
+app.post("/api/register", (req, res) => {
+    
+})
+
+app.get("/api/justify", (req, res) => {
+    
+})
+app.post("/api/justify", (req, res) => {
+    
 })
 
 // Launch server
